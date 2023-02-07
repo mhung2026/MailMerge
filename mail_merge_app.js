4a2aa173b80eba13b1d137e9303340e80a0ae1c1
mhung2026 <124641608+mhung2026@users.noreply.github.com>
2023-02-07 08:16:06 +0700

    Update mail_merge_app.js

--- mail_merge_app.js
+++ mail_merge_app.js
@@ -2,22 +2,24 @@
 
 
 function Mail_Merge_App(){
-  let mail_title = "Tiêu đề của email";
+  let mail_title = "TEST MAIL";
   let first = 0;
   let last = 1;
-  let email = 2;
+  let sex = 2;
+  let email = 3;
   let emailTemp = HtmlService.createTemplateFromFile("index");
   let ws = SpreadsheetApp.openById("1WgBk2TiWY945u_Va-eLHZn7c3CfInp_Tl4C4JULcS8c") // <-- ID của Sheet
-  let data = ws.getRange("A2:C" + ws.getLastRow()).getValues(); 
+  let data = ws.getRange("A2:D" + ws.getLastRow()).getValues(); 
   data.forEach(function(row){
       emailTemp.fn = row[first];
       emailTemp.ln = row[last];
+      if (row[sex]=="Nam") {emailTemp.sn = "anh";} else emailTemp.sn = "chị";
       let htmlMessage = emailTemp.evaluate().getContent();
       GmailApp.sendEmail(
         row[email], 
         mail_title , //+ ', ' + row[first] + '!',
         "Your email doesn't support HTML.",
-        {name: "Email gửi", htmlBody: htmlMessage} 
+        {name: "ngominhhung208@gmail.com", htmlBody: htmlMessage} 
         );        
       }
   );   
